086f628cbc119db7ba48808c473f6ea2ea859e0a
genius-cabana <1513661@senati.pe>
2025-06-25 15:09:53 -0500

    Subiendo los archivos

--- public/login.php
+++ public/login.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html>
 <head>
     <title>Login</title>
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
 <h2>Iniciar Sesión</h2>
